3aafb07237339022da36c741f2481e19abb8e13a
Sufiya Mumtaz <sufiyascript@gmail.com>
2026-03-07 21:10:36 +0530

    Create string_formatting.py

---
+++ string_formatting.py
@@ -0,0 +1,16 @@
+def print_formatted(number):
+    width = len(bin(number)) - 2
+    for i in range(1, number + 1):
+        decimal = str(i).rjust(width)
+        octal = oct(i)[2:].rjust(width)
+        hexa = hex(i)[2:].upper().rjust(width)
+        binary = bin(i)[2:].rjust(width)
+        
+        print(decimal, octal, hexa, binary)
+
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)

e1c01e38a26e514080bff729991349edc364de56
Sufiya Mumtaz <sufiyascript@gmail.com>
2026-03-23 22:17:28 +0530

    Create set_add.py

---
+++ set_add.py
@@ -0,0 +1,9 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+countries = set()
+
+for _ in range(n):
+    countries.add(input())
+
+print(len(countries))

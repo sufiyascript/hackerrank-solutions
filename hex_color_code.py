5c2d084863ab3a23b57dfafa6cb9ad3d3abfbd5e
Sufiya Mumtaz <sufiyascript@gmail.com>
2026-05-01 20:40:10 +0530

    Add functionality to find hex color codes
    Extract hex color codes from input lines within braces.

---
+++ hex_color_code.py
@@ -0,0 +1,19 @@
+import re
+
+n = int(input())
+
+inside_block = False
+
+for _ in range(n):
+    line = input()
+    
+    if '{' in line:
+        inside_block = True
+    if '}' in line:
+        inside_block = False
+    
+    if inside_block:
+        matches = re.findall(r'#[0-9a-fA-F]{3,6}', line)
+        
+        for m in matches:
+            print(m)

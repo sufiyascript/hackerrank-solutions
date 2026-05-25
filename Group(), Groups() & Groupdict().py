a40a6f783337a53bf64adcb830d74babcac4a96a
Sufiya Mumtaz <sufiyascript@gmail.com>
2026-05-25 08:27:19 +0530

    Create Group(), Groups() & Groupdict().py

---
+++ Group(), Groups() & Groupdict().py
@@ -0,0 +1,12 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+import re
+
+s = input()
+
+match = re.search(r'([a-zA-Z0-9])\1+', s)
+
+if match:
+    print(match.group(1))
+else:
+    print(-1)

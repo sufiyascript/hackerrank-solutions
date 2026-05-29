95f267459a67ebda8a7112ee757ae1458f6281fd
Sufiya Mumtaz <sufiyascript@gmail.com>
2026-05-29 19:15:28 +0530

    Create Map and Lambda Function.py

---
+++ Map and Lambda Function.py
@@ -0,0 +1,16 @@
+cube = lambda x: x**3
+
+def fibonacci(n):
+   fib = []
+   a, b = 0, 1
+
+   for i in range(n):
+     fib.append(a)
+     a, b = b, a + b
+
+   return fib
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print(list(map(cube, fibonacci(n))))

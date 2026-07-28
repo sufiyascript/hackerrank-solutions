1c4446ab50e949c40f90ce42408348aa705eddcb
Sufiya Mumtaz <sufiyascript@gmail.com>
2026-07-28 20:03:37 +0530

    Create Symmetric Pairs.sql

---
+++ Symmetric Pairs.sql
@@ -0,0 +1,16 @@
+SELECT DISTINCT f1.X, f1.Y
+FROM Functions f1
+JOIN Functions f2
+ON f1.X = f2.Y
+AND f1.Y = f2.X
+WHERE f1.X < f1.Y
+
+UNION
+
+SELECT X, Y
+FROM Functions
+WHERE X = Y
+GROUP BY X, Y
+HAVING COUNT(*) > 1
+
+ORDER BY X;
